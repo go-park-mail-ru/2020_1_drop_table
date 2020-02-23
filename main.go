78f93770e13480b90a604a22f8f1de6600e0c576
BoldinDmitry <boldin.dmitry@gmail.com>
2020-02-23 19:48:06 +0300

    gorilla routers added, register function code style fixed

--- main.go
+++ main.go
@@ -7,9 +7,9 @@ import (
 	"github.com/go-playground/locales/en"
 	ut "github.com/go-playground/universal-translator"
 	"github.com/gorilla/mux"
+	"github.com/rs/zerolog/log"
 	"gopkg.in/go-playground/validator.v9"
 	enTranslations "gopkg.in/go-playground/validator.v9/translations/en"
-	"log"
 	"net/http"
 	"sync"
 	"time"
@@ -89,20 +89,24 @@ func (ds *OwnersStorage) Count() int {
 	return ds.count()
 }
 
-type Error struct {
+type HttpError struct {
 	Code    int    `json:"code"`
 	Message string `json:"message"`
 }
 
-func createNewError(code int, message string) *Error {
-	return &Error{
+func (e *HttpError) Error() string {
+	return fmt.Sprintf("Error: '%s', with status code: %d", e.Message, e.Code)
+}
+
+func createNewHttpError(code int, message string) *HttpError {
+	return &HttpError{
 		Code:    code,
 		Message: message,
 	}
 }
 
-type ResponseErrorText struct {
-	Errors []Error `json:"errors"`
+type HttpErrorsSlice struct {
+	Errors []HttpError `json:"errors"`
 }
 
 //ToDo refactor function
@@ -139,113 +143,93 @@ func getValidator() (*validator.Validate, ut.Translator, error) {
 	return v, trans, nil
 }
 
-func getValidationErrors(err error, trans ut.Translator) ResponseErrorText {
-	errs := ResponseErrorText{}
-	for _, e := range err.(validator.ValidationErrors) {
-		validationError := createNewError(400, e.Translate(trans))
+func getValidationErrors(err error, trans ut.Translator) []HttpError {
+	errorsCount := len(err.(validator.ValidationErrors))
+	errs := make([]HttpError, errorsCount, errorsCount)
 
-		errs.Errors = append(errs.Errors, *validationError)
+	for i, e := range err.(validator.ValidationErrors) {
+		validationError := createNewHttpError(400, e.Translate(trans))
+		errs[i] = *validationError
 	}
 	return errs
 }
 
-func registerView(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "POST":
-		jsonData := r.FormValue("jsonData")
-		if jsonData == "" {
-			errs := ResponseErrorText{}
-			errs.Errors = append(errs.Errors, Error{400, "empty jsonData field"})
-			serializedError, err := json.Marshal(errs)
-			if err != nil {
-				w.WriteHeader(500)
-				return
-			}
+func sendServerError(errorMessage string, w http.ResponseWriter) {
+	log.Error().Msg(errorMessage)
+	w.WriteHeader(500)
+}
 
-			_, err = w.Write(serializedError)
-			if err != nil {
-				w.WriteHeader(500)
-				return
-			}
-			return
-		}
-		ownerObj := Owner{CreatedAt: time.Now()}
+func sendSingleError(errorMessage string, w http.ResponseWriter) {
+	errs := make([]HttpError, 1, 1)
+	errs[0] = HttpError{400, errorMessage}
+	sendSeveralErrors(errs, w)
+}
 
-		if err := json.Unmarshal([]byte(jsonData), &ownerObj); err != nil {
-			errs := ResponseErrorText{}
-			errs.Errors = append(errs.Errors, Error{400, "json parsing error"})
-			serialized, _ := json.Marshal(errs)
-			_, err = w.Write(serialized)
-			if err != nil {
-				w.WriteHeader(500)
-				return
-			}
-			fmt.Println("Unmarshal error: ", err)
-			return
-		}
-
-		validation, trans, validatorError := getValidator()
-
-		if validatorError != nil {
-			w.WriteHeader(500)
-			return
-		}
-
-		if err := validation.Struct(ownerObj); err != nil {
-			errs := getValidationErrors(err, trans)
-			serialized, err := json.Marshal(errs)
-			if err != nil {
-				w.WriteHeader(500)
-				return
-			}
-
-			_, err = w.Write(serialized)
-			if err != nil {
-				w.WriteHeader(500)
-				return
-			}
-			return
-		}
-		err := owners.Append(ownerObj)
-		if err != nil {
-			errs := ResponseErrorText{}
-			errs.Errors = append(errs.Errors, Error{400, "User with this email already existed"})
-			serialized, _ := json.Marshal(errs)
-			_, err = w.Write(serialized)
-			if err != nil {
-				w.WriteHeader(500)
-				return
-			}
-		}
-		owners.Print()
-
-	default:
-		methodErr := ResponseErrorText{}
-		methodErr.Errors = append(methodErr.Errors, Error{400, "this method is unavailable"})
-		serialized, _ := json.Marshal(methodErr)
-		_, err := w.Write(serialized)
-		if err != nil {
-			w.WriteHeader(500)
-			return
-		}
+func sendSeveralErrors(errors []HttpError, w http.ResponseWriter) {
+	errs := HttpErrorsSlice{}
+	for _, err := range errors {
+		errs.Errors = append(errs.Errors, err)
+	}
+	serializedError, err := json.Marshal(errs)
+	if err != nil {
+		message := fmt.Sprintf("HttpError is json serializing: %s", err.Error())
+		sendServerError(message, w)
 		return
 	}
-	return
 
+	_, err = w.Write(serializedError)
+	if err != nil {
+		message := fmt.Sprintf("HttpError while writing is socket: %s", err.Error())
+		sendServerError(message, w)
+		return
+	}
+}
+
+func registerView(w http.ResponseWriter, r *http.Request) {
+	jsonData := r.FormValue("jsonData")
+	if jsonData == "" {
+		sendSingleError("empty jsonData field", w)
+		return
+	}
+	ownerObj := Owner{CreatedAt: time.Now()}
+
+	if err := json.Unmarshal([]byte(jsonData), &ownerObj); err != nil {
+		sendSingleError("json parsing error", w)
+		return
+	}
+
+	validation, trans, err := getValidator()
+	if err != nil {
+		message := fmt.Sprintf("HttpError in validator: %s", err.Error())
+		sendServerError(message, w)
+		return
+	}
+
+	if err := validation.Struct(ownerObj); err != nil {
+		errs := getValidationErrors(err, trans)
+		sendSeveralErrors(errs, w)
+		return
+	}
+
+	if err := owners.Append(ownerObj); err != nil {
+		sendSingleError("User with this email already existed", w)
+	}
+	return
 }
 
 var owners = NewOwnersStorage()
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/api/v1/owner", registerView)
+	r.HandleFunc("/api/v1/owner", registerView).Methods("POST")
 	http.Handle("/", r)
-	fmt.Println("starting server at :8080")
+	log.Info().Msg("starting server at :8080")
 	srv := &http.Server{
 		Handler:      r,
 		Addr:         "127.0.0.1:8080",
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
-	log.Fatal(srv.ListenAndServe())
+	log.Error().Msg(srv.ListenAndServe().Error())
+
 }
