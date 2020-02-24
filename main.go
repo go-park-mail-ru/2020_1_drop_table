3b24bad36376fde15c3e1fb5373699fc37f046a3
BoldinDmitry <boldin.dmitry@gmail.com>
2020-02-24 22:53:41 +0300

    Access-Control-Allow-Headers *

--- main.go
+++ main.go
@@ -823,7 +823,7 @@ func MyCORSMethodMiddleware(r *mux.Router) mux.MiddlewareFunc {
 		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 			w.Header().Set("Content-Type", "*")
 			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, csrf-token, Authorization")
+			w.Header().Set("Access-Control-Allow-Headers", "*")
 			w.Header().Set("Access-Control-Allow-Origin", "http://localhost:63342")
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
 			w.Header().Set("Set-Cookie", "*")
