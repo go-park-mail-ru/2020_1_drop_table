e6fc493a265b05ac4c98486ac0b1dee637b87eb9
BoldinDmitry <boldin.dmitry@gmail.com>
2020-03-02 21:03:19 +0300

    Bad headers were deleted (Content-Type, Set-Cookie)

--- main.go
+++ main.go
@@ -904,14 +904,12 @@ func loggingMiddleware(next http.Handler) http.Handler {
 func MyCORSMethodMiddleware(_ *mux.Router) mux.MiddlewareFunc {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-			w.Header().Set("Content-Type", "*")
 			w.Header().Set("Access-Control-Allow-Methods",
 				"POST, GET, OPTIONS, PUT, DELETE")
 			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length,"+
 				" Accept-Encoding, X-CSRF-Token, csrf-token, Authorization")
 			w.Header().Set("Access-Control-Allow-Origin", "http://localhost:63342")
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
-			w.Header().Set("Set-Cookie", "*")
 			w.Header().Set("Vary", "Accept, Cookie")
 			if req.Method == "OPTIONS" {
 				return
