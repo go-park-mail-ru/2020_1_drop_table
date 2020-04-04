a8c275f5a829f2e19d6f021bf2770bdaf8188ba0
dmitrijgulacenkov <pingvin1959@yandex.ru>
2020-04-04 19:32:14 +0300

    panic middleware

---
+++ internal/app/middleware/PanicMiddleware.go
@@ -0,0 +1,19 @@
+package middleware
+
+import (
+	"github.com/rs/zerolog/log"
+	"net/http"
+)
+
+func PanicMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Error().Msgf("panic in " + r.URL.Path)
+		defer func() {
+			if err := recover(); err != nil {
+				log.Error().Msgf("recovered after error")
+				http.Error(w, "Internal server error", 500)
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
