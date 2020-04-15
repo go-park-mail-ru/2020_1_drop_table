f1a0b67ee4304e0bb3263b27c3cdf3e26460ff4c
dmitry <boldin.dmitry@gmail.com>
2020-04-15 23:41:51 +0300

    repo mock fixed update customers_count.go edited

--- internal/pkg/apple_pass_generator/meta/update_functions/customers_count.go
+++ internal/pkg/apple_pass_generator/meta/update_functions/customers_count.go
@@ -1,6 +1,10 @@
 package update_functions
 
 func UpdateVarPassesCount(oldVal interface{}) (interface{}, error) {
+	if oldVal == nil {
+		oldVal = 0
+	}
+
 	oldValInt, ok := oldVal.(float64)
 
 	if !ok {
