b64a3f0749bd7c588da0064ee8525be4e8b94284
dmitrijgulacenkov <pingvin1959@yandex.ru>
2020-04-27 12:36:14 +0300

    new mocks

--- internal/app/cafe/mocks/Repository.go
+++ internal/app/cafe/mocks/Repository.go
@@ -35,6 +35,29 @@ func (_m *Repository) Add(ctx context.Context, ca models.Cafe) (models.Cafe, err
 	return r0, r1
 }
 
+// GetAllCafes provides a mock function with given fields: ctx, since, limit
+func (_m *Repository) GetAllCafes(ctx context.Context, since int, limit int) ([]models.Cafe, error) {
+	ret := _m.Called(ctx, since, limit)
+
+	var r0 []models.Cafe
+	if rf, ok := ret.Get(0).(func(context.Context, int, int) []models.Cafe); ok {
+		r0 = rf(ctx, since, limit)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]models.Cafe)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, int, int) error); ok {
+		r1 = rf(ctx, since, limit)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // GetByID provides a mock function with given fields: ctx, id
 func (_m *Repository) GetByID(ctx context.Context, id int) (models.Cafe, error) {
 	ret := _m.Called(ctx, id)
--- internal/app/cafe/mocks/Usecase.go
+++ internal/app/cafe/mocks/Usecase.go
@@ -35,6 +35,29 @@ func (_m *Usecase) Add(c context.Context, newCafe models.Cafe) (models.Cafe, err
 	return r0, r1
 }
 
+// GetAllCafes provides a mock function with given fields: ctx, since, limit
+func (_m *Usecase) GetAllCafes(ctx context.Context, since int, limit int) ([]models.Cafe, error) {
+	ret := _m.Called(ctx, since, limit)
+
+	var r0 []models.Cafe
+	if rf, ok := ret.Get(0).(func(context.Context, int, int) []models.Cafe); ok {
+		r0 = rf(ctx, since, limit)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]models.Cafe)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, int, int) error); ok {
+		r1 = rf(ctx, since, limit)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // GetByID provides a mock function with given fields: c, id
 func (_m *Usecase) GetByID(c context.Context, id int) (models.Cafe, error) {
 	ret := _m.Called(c, id)
