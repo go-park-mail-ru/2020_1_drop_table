7bfee1c7f336d677810ac3e65a950ca855e95dc3
dmitry <boldin.dmitry@gmail.com>
2020-05-15 02:29:07 +0300

    GRPC in docker fixed images volumes fixed

--- configs/config_file.go
+++ configs/config_file.go
@@ -21,9 +21,9 @@ const AppleKey = "configs/apple_config_files/passkey.pem"
 //on urls with this ports frontend need to send data
 const MainService = "0.0.0.0:8080"
 const HTTPStaffUrl = "0.0.0.0:8084"
-const GRPCStaffUrl = "0.0.0.0:8083"
+const GRPCStaffUrl = "staff:8083"
 const HTTPSurveyUrl = "0.0.0.0:8086"
-const GRPCCafeUrl = "0.0.0.0:8085"
+const GRPCCafeUrl = "main_service:8085"
 const GRPCCustomerUrl = "0.0.0.0:8082"
 
 var ApplePassword = os.Getenv("ApplePassword")
