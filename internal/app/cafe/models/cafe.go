20a4b8a177351a6e91018436956d03340e117406
dmitrijgulacenkov <pingvin1959@yandex.ru>
2020-05-17 18:19:40 +0300

    deleted unused struct

--- internal/app/cafe/models/cafe.go
+++ internal/app/cafe/models/cafe.go
@@ -4,20 +4,10 @@ import (
 	"time"
 )
 
+//easyjson -all cafe.go
+
 //easyjson:json
 type Cafe struct {
-	CafeID      int       `json:"id"`
-	CafeName    string    `json:"name" validate:"required,min=2,max=100"`
-	Address     string    `json:"address" validate:"required"`
-	Description string    `json:"description" validate:"required"`
-	StaffID     int       `json:"staffID"`
-	OpenTime    time.Time `json:"openTime"`
-	CloseTime   time.Time `json:"closeTime"`
-	Photo       string    `json:"photo"`
-	Location    string    `json:"location" db:"location_str"`
-}
-
-type CafeWithGeoData struct {
 	CafeID      int       `json:"id"`
 	CafeName    string    `json:"name" validate:"required,min=2,max=100"`
 	Address     string    `json:"address" validate:"required"`
